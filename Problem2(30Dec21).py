0a0206882adba2a9708170a87e11e4f2f3538d74
John241198 <96462147+John241198@users.noreply.github.com>
2021-12-31 09:21:35 +0530

    Problem2(30Dec21) uploaded

---
+++ Problem2(30Dec21).py
@@ -0,0 +1,20 @@
+"""
+Problem 2:
+In a sentence, find out if there is a valid IP address and print the same.
+Try solution with and without usage of regular expressions.
+For example:
+Input:
+"The system's is accessed from 10.50.200.1"
+output :
+IP Address is 10.50.200.1
+Total IP Address noted is 1
+"""
+ip="IP Address is 10.50.200.1 and also along with 10.50.200.1 also along with 10.50.200.1 also along with 10.50.200.1"
+
+
+new_ip = ''.join((i if i in '0123456789.' else ' ') for i in ip)
+ip_list=[]
+for i in new_ip.split():
+    ip_list.append(i)
+    print("IP Address is",i)
+print("\nTotal IP address noted is",len(ip_list))
