744a9c9b4b6410a5ea67969aa7500feb1a84c679
John241198 <96462147+John241198@users.noreply.github.com>
2022-01-04 19:40:48 +0530

    Update Problem2(03Jan22).py

--- Problem2(03Jan22).py
+++ Problem2(03Jan22).py
@@ -70,3 +70,73 @@ task1.join()
 task2.join()
 
 print("Completed!....")
+
+
+#execution time count is added
+#without thread
+import time
+
+start=time.perf_counter()
+
+class Hello:
+
+    def run(self):
+        for i in range(5):
+            print("hello")
+
+
+
+class Hi:
+
+    def run(self):
+        for i in range(5):
+            print("hi")
+
+
+task1=Hello()
+task2=Hi()
+
+task1.run()
+task2.run()
+
+finish=time.perf_counter()
+print(f"finished in {round(finish-start,2)} second(s)")
+
+#with thread module
+from time import sleep
+from threading import *
+import time
+
+start=time.perf_counter()
+
+class Hello(Thread):    # fun's sub-class//thread -need to import module "from threading import *"
+
+    def run(self):        #run is inbuild method in thread if we call any other method it will not work 
+        for i in range(5):
+            print("hello")
+            #sleep(1)      #without sleep it work too faster so we make sleep to print one fun to other fun with sleep()
+                           
+
+
+class Hi(Thread):
+
+    def run(self):
+        for i in range(5):
+            print("hi")
+            #sleep(1)
+
+
+task1=Hello()
+task2=Hi()
+
+task1.start()   #if we want to run two different thread instant of calling run method we neet to call start()-method //when we call start() internaly it call run method
+#sleep(0.2) #sometime two thread come in the cpu at same time and it will make collision
+task2.start()   
+
+
+task1.join()
+task2.join()
+
+print("Completed!....")
+finish=time.perf_counter()
+print(f"finished in {round(finish-start,2)} second(s)")
