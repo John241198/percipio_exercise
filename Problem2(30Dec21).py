5ff638141bae8b61d6a9b8482df297a29e24d000
John241198 <96462147+John241198@users.noreply.github.com>
2022-01-04 19:37:00 +0530

    Updated ip_address Problem2(30Dec21)

--- Problem2(30Dec21).py
+++ Problem2(30Dec21).py
@@ -27,3 +27,29 @@ ip_search=re.findall("\d{2}.\d{2}.\d{3}.\d{1}",ip)
 for i in ip_search:
     print("IP Address is",i)
 print("\nTotal IP address noted is",len(ip_search))
+
+
+
+#updated program
+
+import re
+ip="""IP Address is 10.50.200.1 and also 11.11.500.6 along with 10.50.200.1 also along with 10.50.200.1 also along with 10.50.200.1 along with 10.50.200.1 
+    along with 10.50.200.1"""
+new_ip = ''.join((i if i in '0123456789.' else ' ') for i in ip)
+ip_list=[]
+for i in new_ip.split():
+    ip_list.append(i)
+    
+#print(ip_list)
+regex_pattern = "^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$"
+
+count=0
+for i in ip_list:
+    result = bool( re.match( regex_pattern,i))
+    if (result):
+        print("IP Address is:",i)
+        count+=1
+    #else:
+        #print("Invalid")
+
+print("\nTotal IP address noted is",count)
